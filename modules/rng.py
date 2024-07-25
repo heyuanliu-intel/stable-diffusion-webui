5ec711b0fbf79ef4dea4e6040cba0f6a928897d2
Heyuan Liu <heyuan.liu@intel.com>
2024-07-25 22:50:00 +0800

    fix error: Device type HPU is not supported for torch.Generator() api

--- modules/rng.py
+++ modules/rng.py
@@ -73,6 +73,9 @@ def manual_seed(seed):
 
 
 def create_generator(seed):
+    if shared.cmd_opts.use_hpu:
+        return torch.Generator().manual_seed(int(seed))
+    
     if shared.opts.randn_source == "NV":
         return rng_philox.Generator(seed)
 
